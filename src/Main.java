86d4cc8f74fda432c4d2ab4cee16dbc6308ac1da
amenbruhh <pachison.artyom@yandex.ru>
2024-07-06 19:22:40 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ public class Main {
 
         // задача 1
 
-        String firstName = "Ivan ";
-        String middleName = "Ivanovich ";
-        String lastName = "Ivanov ";
-        String fullName = lastName + firstName + middleName;
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
 
         System.out.println("Ф. И. О. сотрудника — " + fullName);
         System.out.println();
